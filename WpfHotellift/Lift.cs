ddc32b50b17b5a0c367c1912bbc9d919bedcc101
pappferencgabor <70635774+pappferencgabor@users.noreply.github.com>
2023-11-14 16:28:21 +0100

    elkészült

--- WpfHotellift/Lift.cs
+++ WpfHotellift/Lift.cs
@@ -8,6 +8,23 @@ namespace WpfHotellift
 {
     internal class Lift
     {
+        private string idopont;
+        private int kartyaszam;
+        private int induloszint;
+        private int celszint;
 
+        public Lift(string sor)
+        {
+            var temp = sor.Split(" ");
+            Idopont = temp[0];
+            Kartyaszam = int.Parse(temp[1]);
+            Induloszint = int.Parse(temp[2]);
+            Celszint = int.Parse(temp[3]);
+        }
+
+        public string Idopont { get => idopont; set => idopont = value; }
+        public int Kartyaszam { get => kartyaszam; set => kartyaszam = value; }
+        public int Induloszint { get => induloszint; set => induloszint = value; }
+        public int Celszint { get => celszint; set => celszint = value; }
     }
 }
--- WpfHotellift/MainWindow.xaml.cs
+++ WpfHotellift/MainWindow.xaml.cs
@@ -21,14 +21,65 @@ namespace WpfHotellift
     /// </summary>
     public partial class MainWindow : Window
     {
+        static List<Lift> liftek = new List<Lift>();
+        int celszint;
+        int kartyaszam;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            File.ReadAllLines("lift.txt").ToList().ForEach(x => liftek.Add(new Lift(x)));
         }
 
-        private void btnFeladat2_Click(object sender, RoutedEventArgs e)
+        private void btnFeladat3_Click(object sender, RoutedEventArgs e)
         {
-            var elemek = File.ReadAllLines("lift.txt");
+            MessageBox.Show($"3. feladat: Összes lifthasználat: {liftek.Count}");
+        }
+
+        private void btnFeladat4_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"4. feladat: Időszak: {liftek[0].Idopont} - {liftek[liftek.Count - 1].Idopont}");
+        }
+
+        private void btnFeladat5_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"5. feladat: Célszint max: {liftek.Max(x => x.Celszint)}");
+        }
+
+        private void btnFeladat6_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                kartyaszam = int.Parse(txtKartya.Text);
+                celszint = int.Parse(txtCelszint.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Hibásan megadott adat");
+                celszint = 5;
+            }
+            MessageBox.Show($"6. feladat:\nKártya száma: {kartyaszam}\nCélszint száma: {celszint}");
+        }
+
+        private void btnFeladat7_Click(object sender, RoutedEventArgs e)
+        {
+            int utazasSzam = liftek.Where(x => x.Celszint == celszint).Count(x => x.Kartyaszam == kartyaszam);
+
+            MessageBox.Show(utazasSzam == 0
+                ? $"7. feladat: A(z) {kartyaszam}. kártyával nem utaztak a(z) {celszint}. emeletre!"
+                : $"7. feladat: A(z) {kartyaszam}. kártyával utaztak a(z) {celszint}. emeletre!");
+        }
+
+        private void btnFeladat8_Click(object sender, RoutedEventArgs e)
+        {
+            var csoportos = liftek.GroupBy(x => x.Idopont);
+            string kiiratas = "8. feladat: Statisztika\n";
+            foreach (var datum in csoportos)
+            {
+                kiiratas += $"{datum.Key} - {liftek.Where(x => x.Idopont == datum.Key).Count()}x\n";
+            }
+            MessageBox.Show(kiiratas);
         }
     }
 }
